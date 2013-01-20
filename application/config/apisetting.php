0b511843242f88e4d77387843359fae8f06a2a90
jasonhotsauce <wenbinzhang0802@gmail.com>
2013-01-20 00:22:05 -0500

    Migrate to Drive v2#

--- application/config/apisetting.php
+++ application/config/apisetting.php
@@ -3,13 +3,13 @@ $config['google_api'] = array(
 
         'client_id' => '', // Your app id
 
-        'client_secret' => '', // Your secret
+        'client_secret' => '', //  Your secret
 
         'scopes' => 'https://www.googleapis.com/auth/drive.file https://spreadsheets.google.com/feeds/ https://docs.googleusercontent.com/',
 
         'app_name' => 'Google Docs API Demo',
 
-        'redirect_uri' => 'http://localhost/~jasonhotsauce/Google_Docs/index.php/home/authorized',
+        'redirect_uri' => '', //  Redirect url after gaining taken
 
         'chunk_size' => 256*1024,
 
